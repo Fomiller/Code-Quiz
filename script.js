f20fef4193b02785da40931ffbbf1711080e3ab1
Forrest Miller <Forrestmillerj@gmail.com>
2020-02-27 23:46:42 -0600

    created endgame function for gathering user initials and score. having problems with the submit button.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ $(document).ready(function(){
     var answerBtn3 = $("#answerBtn3");
     var answerBtn4 = $("#answerBtn4");
     var questionTitle = $("#question-title");
+    // var submitButton = $("#submitBtn");
 
     var gameObject = [
         {
@@ -27,8 +28,7 @@ $(document).ready(function(){
             "answer03": "Hyper Text Mark Language",
             "answer04": "Happy Tree Making Language",
             "correct": ["Hyper Text Mark-up Language"]
-            // how do i reference answer01 in the position of correct[0] in a shorter hand notation
-            //  I think that turning my nested objects into arrays would be easier to loop through
+
         },
         {
             "id": "q02",
@@ -60,57 +60,61 @@ $(document).ready(function(){
         },
         {
             "id": "q05",
-            "question": "What is the DOM?",
-            "answer01": "Doing Optimal Mechanics",
-            "answer02": "Document Object Model",
-            "answer03": "Data Object Model",
-            "answer04": "Directorate of Material",
-            "correct": ["Document Object Model"],
+            "question": "NaN stands for?",
+            "answer01": "Not a numeral",
+            "answer02": "Not a number",
+            "answer03": "Not a negative",
+            "answer04": "Not a node",
+            "correct": ["Not a number"],
         },
         {
             "id": "q06",
-            "question": "What is the DOM?",
-            "answer01": "Doing Optimal Mechanics",
-            "answer02": "Document Object Model",
-            "answer03": "Data Object Model",
-            "answer04": "Directorate of Material",
-            "correct": ["Document Object Model"],
+            "question": "What does JSON mean?",
+            "answer01": "Jason",
+            "answer02": "Jquery Super Object Notation",
+            "answer03": "JavaScript Object Notation",
+            "answer04": "Joing String Object Notation",
+            "correct": ["JavaScript Object Notation"],
         },
         {
             "id": "q07",
-            "question": "What is the DOM?",
-            "answer01": "Doing Optimal Mechanics",
-            "answer02": "Document Object Model",
-            "answer03": "Data Object Model",
-            "answer04": "Directorate of Material",
-            "correct": ["Document Object Model"],
+            "question": "How can you include a script tag?",
+            "answer01": "Inside a link tag",
+            "answer02": "Inside a head tag",
+            "answer03": "Iniside a script tag",
+            "answer04": "Inside the body tag",
+            "correct": ["Iniside a script tag"],
         },
         {
             "id": "q08",
-            "question": "What is the DOM?",
-            "answer01": "Doing Optimal Mechanics",
-            "answer02": "Document Object Model",
-            "answer03": "Data Object Model",
-            "answer04": "Directorate of Material",
-            "correct": ["Document Object Model"],
+            "question": "API is an acronym for",
+            "answer01": "Application Program Interface",
+            "answer02": "Application Programming Interface",
+            "answer03": "Application Proprietary Interface",
+            "answer04": "Application Programing Interval",
+            "correct": ["Application Programming Interface"],
         },
         {
             "id": "q09",
-            "question": "What is the DOM?",
-            "answer01": "Doing Optimal Mechanics",
-            "answer02": "Document Object Model",
-            "answer03": "Data Object Model",
-            "answer04": "Directorate of Material",
-            "correct": ["Document Object Model"],
+            "question": ".split(' ')",
+            "answer01": "Split a string into an array of substrings",
+            "answer02": "Split a string into an array of arrays",
+            "answer03": "Split a string into an array of objects",
+            "answer04": "Split a array into an string of substrings",
+            "correct": ["Split a string into an array of substrings"],
         },
         {
             "id": "q10",
-            "question": "What is the DOM?",
-            "answer01": "Doing Optimal Mechanics",
+            "question": "Jquery is a JavaScript ______.",
+            "answer01": "API",
             "answer02": "Document Object Model",
-            "answer03": "Data Object Model",
-            "answer04": "Directorate of Material",
-            "correct": ["Document Object Model"],
+            "answer03": "Array",
+            "answer04": "Framework",
+            "correct": ["Framework"],
+        },
+        {
+            "id": "Game Over",
+            "title": ["All done!", "Time's up! Game Over!",],
         },
     ];
 
@@ -127,6 +131,17 @@ $(document).ready(function(){
         $("#quiz-title").empty();
     }
 
+    var clearHTMLMax = function() {
+        $("#quiz-title").empty();
+        $("#quiz-description").empty();
+        $("#question-title").empty();
+        $("#quiz-title").empty();
+        $("#answerBtn1").remove();
+        $("#answerBtn2").remove();
+        $("#answerBtn3").remove();
+        $("#answerBtn4").remove();
+    }
+
     // Removes start button
     var showGameBtns = function () {
         answerBtn1.removeClass("invisible");
@@ -138,6 +153,7 @@ $(document).ready(function(){
     
     // renders question info
     var renderQuestion = function (){
+        if (globalVariable < 10){
         // creates title text
         questionTitle.text(gameObject[globalVariable].question);
         // fills answer 1 text
@@ -148,6 +164,12 @@ $(document).ready(function(){
         answerBtn3.text(gameObject[globalVariable].answer03);
         // fills answer 1 text
         answerBtn4.text(gameObject[globalVariable].answer04);
+        } else {
+            // clearHTMLMax();
+            // clearInterval(timer);
+            gameFinish();
+        }
+
     }
     
     var increaseGlobalV = function(){
@@ -190,7 +212,7 @@ $(document).ready(function(){
             console.log(sec)
             sec--;
             if (sec < 0) {
-                clearInterval(timer);
+                timesUp();
             }
         }, 1000);
     }
@@ -200,9 +222,27 @@ $(document).ready(function(){
         sec = sec - 10;
         countdownTimer();
     }
-    
 
-    
+    // Run if you make it through all questions without running out of time displays a congratulations title 
+    var gameFinish = function() {
+        clearInterval(timer);
+        clearHTMLMax();
+        $("#quiz-title").text(gameObject[10].title[0])
+        $("#question-title").text("You scored: " + score + "/10");
+        createInitInput();
+
+    }
+
+    // Only run when time seconds becomes less then 0, called in the time interval
+    var timesUp = function() {
+        clearInterval(timer);
+        clearHTMLMax();
+        $("#quiz-title").text(gameObject[10].title[1])
+        $("#question-title").text("You scored: " + score + "/10");
+        createInitInput();
+    }
+
+
     var startGame = function() {
         console.log("Game Running");
         countdownTimer();
@@ -212,19 +252,51 @@ $(document).ready(function(){
         renderQuestion();
         
     };
-    
-    
-    
-        // removes start button and renders the first title tag
-        // function runGame(){
-        //     removeStartBtn();
-        //     // console.log(gameObject[globalVariable]);
-        // }
-    
-    // compareAnswer();
 
+    var createInitInput = function(){
+        var textInput = $('<input/>');
+        var subBtn = $('<button>');
+        subBtn.attr("id", 'submitBtn')
+        subBtn.attr("class", 'btn btn-success')
+        subBtn.text("Submit")
+        textInput.attr({ type: 'text', id: 'init-input',});
+        textInput.appendTo($("#content"));
+        subBtn.append($("#content"));
+    }
 
+    var displayMessage = function(){
+        alert("Players' initials can not be blank.")
+    }
+
+    var storePlayer = function(event){
+        event.preventDefault();
+
+        console.log("store player")
+        // console.log($("#initial-input").value.trim());
+
+        var player = {
+            name: "forrest",
+            name: $("#init-input").value.trim(),
+            score: score,
+        };
+
+        if (player.name ===""){
+            displayMessage();
+        }
+
+        console.log(player)
+        localStorage.setItem("player", JSON.stringify(player));
+    }
+
+    var sub = function(){
+        console.log($("#init-input").value.trim())
+    }
+    
+    
+    // localStorage.setItem({"Player Name", "Forrest" "Score", "10",})
+
+    $("#submitBtn").on("click", sub);
     $("#startBtn").on("click", startGame);
-    $(".gameBtn").on("click", gameBtn)
+    $(".gameBtn").on("click", gameBtn);
 
 });
